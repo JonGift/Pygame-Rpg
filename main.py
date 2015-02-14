b59928aa3e160723f021f76dfb247bf740e814c3
Yamist <8bityamist@gmail.com>
2015-02-14 13:08:00 -0800

    many changes with a picture

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ fps_clock = pygame.time.Clock()
 pygame.init()
 display_surface = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 pygame.display.set_caption('An rpg')
+temp = pygame.image.load('img/blank_tile.png')
 
 while True:
-
+    display_surface.blit(temp, (0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
