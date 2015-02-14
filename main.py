052941a1e43c76a70546417a78c9739ac9291e2e
Yamist <8bityamist@gmail.com>
2015-02-14 13:43:03 -0800

    Added tile creation.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 __author__ = 'Jawnathan'
-import constants
 import pygame
 import sys
+
+import constants
+import tiles
+import player
 
 fps_clock = pygame.time.Clock()
 pygame.init()
 display_surface = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 pygame.display.set_caption('An rpg')
 temp = pygame.image.load('img/blank_tile.png')
+temp = tiles.map(25,20,'temp')
 
 while True:
-    display_surface.blit(temp, (0,0))
+    temp.update(display_surface)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
--- tiles.py
+++ tiles.py
@@ -1,17 +1,42 @@
+import pygame
 import constants
 
 class tile:
-    def __init__(self, tile_size, type):
+    def __init__(self, type, tile_size):
+        self.type = type
+        self.tile_size = tile_size
+        self.x = 0
+        self.y = 0
+        self.reference_name = 0
+        if self.type == 0:
+            self.type = 'blank_tile'
 
-def create_tile(pos_x, pos_y, tile_size = constants.TILE_SIZE, type):
+def create_tile(pos_x, pos_y, type, reference_number, tile_size = constants.TILE_SIZE):
+    new_tile = tile(type, tile_size)
+    new_tile.type = pygame.image.load('img/' + new_tile.type + '.png')
+    new_tile.x = pos_x * 40
+    new_tile.y = pos_y * 40
+    new_tile.reference_name = reference_number
 
+    return new_tile
 
 class map:
     def __init__(self, length, width, effect):
         self.length = length
         self.width = width
         self.effect = effect
+        #tile dict here, need to update tiles.
+        self.tile_dictionary = {}
+        self.counter = 0
 
         for x in range(self.length):
             for i in range(self.width):
-                create_tile(x, i)
+                self.temp = create_tile(x, i, 0, self.counter)
+                self.tile_dictionary[self.temp.reference_name] = create_tile(x, i, 0, self.counter)
+                self.counter += 1
+
+    def update(self, display_surface):
+        #Update tiles here!
+        for i in range(len(self.tile_dictionary)):
+            display_surface.blit(self.tile_dictionary[i].type, (self.tile_dictionary[i].x, self.tile_dictionary[i].y))
+
