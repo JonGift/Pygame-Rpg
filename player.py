1a7ae947a5d9e031b79670a74c38bb935bb035cc
Brett Woodske <brett.woodske@student.ufv.ca>
2015-02-14 13:38:33 -0800

    many changes

--- player.py
+++ player.py
@@ -9,7 +9,13 @@ class Player():
         self.health = 100
         self.mana = 20
 
-        self.sprite =
+        self.sprite = pygame.Surface([constants.TILE_SIZE, constants.TILE_SIZE])
+        self.sprite.fill(constants.BLUE)
+
+        self.rect = self.sprite.get_rect()
+
+        #Lists which way the player is facing: N=0, W=1, S=2, E=3
+        self.dir_facing = 0
 
     def set_location(self, x, y):
         self.x_pos = x
@@ -18,11 +24,20 @@ class Player():
     def move(self, direction):
         if direction == "down":
             self.y_pos += constants.TILE_SIZE
+            self.dir_facing = 2
+
         elif direction == "up":
             self.y_pos -= constants.TILE_SIZE
+            self.dir_facing = 0
+
         elif direction == "left":
             self.x_pos -= constants.TILE_SIZE
+            self.dir_facing = 1
+
         elif direction == "right":
             self.x_pos += constants.TILE_SIZE
+            self.dir_facing = 3
 
     def draw(self):
+        gameDisplay.blit(self.sprite, (self.x_pos, self.y_pos))
+
