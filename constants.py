906e61d127f4c5f3e02cf12784e04d3083e0e9e9
Brett Woodske <brett.woodske@student.ufv.ca>
2015-02-14 12:22:06 -0800

    added constants.py

---
+++ constants.py
@@ -0,0 +1,28 @@
+"""
+Global constants
+"""
+
+# Colors
+BLACK    = (   0,   0,   0) 
+WHITE    = ( 255, 255, 255) 
+BLUE     = (   0,   0, 255)
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+GREY = (200,200,200)
+
+RED = (200,0,0)
+LIGHT_RED = (255,0,0)
+
+BLUE = (0,0,255)
+
+YELLOW = (200,200,0)
+LIGHT_YELLOW = (255,255,0)
+
+GREEN = (0,155,0)
+LIGHT_GREEN = (0,255,0)
+
+# Screen dimensions
+SCREEN_WIDTH  = 1024
+SCREEN_HEIGHT = 768
