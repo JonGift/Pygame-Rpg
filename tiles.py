5e8b66ed480c6ed9ddc09ef1dbf00f1838951457
Yamist <8bityamist@gmail.com>
2015-02-14 13:12:33 -0800

    Many changes with a picture.

--- tiles.py
+++ tiles.py
@@ -3,7 +3,7 @@ import constants
 class tile:
     def __init__(self, tile_size, type):
 
-def create_tile(x, y, tile_size = constants.TILE_SIZE, type):
+def create_tile(pos_x, pos_y, tile_size = constants.TILE_SIZE, type):
 
 
 class map:
